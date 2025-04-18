199147ba48f26c5f684b1e2fa7f1e91f17634c38
BazzoFX <bazzofx@outlook.com>
2025-04-18 14:44:30 +0100

    Ignoring Hydration issues on theme render

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,13 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {/* Flying emails at the root level */}
           <FlyingEmails />
           {children}
         </ThemeProvider>
+        
       </body>
     </html>
   )
